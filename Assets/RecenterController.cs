b04088a189037d1ca459626f8f3ba7b28479f62c
Tagrost <tagrost@gmail.com>
2016-06-05 20:42:28 -0500

    Added Recenter feature
    added recenter feature on OVR Rig Network

---
+++ Assets/RecenterController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.VR;
+
+public class RecenterController : MonoBehaviour
+{
+
+    void Start()
+    {
+        UnityEngine.VR.InputTracking.Recenter();
+    }
+
+    void Update()
+    {
+        if (OVRInput.Get(OVRInput.Button.PrimaryThumbstick))
+        {
+            UnityEngine.VR.InputTracking.Recenter();
+        }
+    }
+}
