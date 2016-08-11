ee0771195fae82944768562871b1632e805a9f43
drohack <drohack@gmail.com>
2016-08-11 18:38:00 -0500

    Updated toys to be networked
    Added the Photon View to all of the toys and added a script that updates
    their position and rotation.

---
+++ Assets/Photon Lobby/Photon_Transform_Rotation_Script.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Photon_Transform_Rotation_Script : Photon.MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start() {
+        
+    }
+
+    void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            //We own this player: send the others our data
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
+        }
+        else
+        {
+            //Network player, receive data
+            pos = (Vector3)stream.ReceiveNext();
+            rot = (Quaternion)stream.ReceiveNext();
+        }
+    }
+
+    private Vector3 pos = Vector3.zero; //We lerp towards this
+    private Quaternion rot = Quaternion.identity; //We lerp towards this
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!photonView.isMine)
+        {
+            //Update remote player (smooth this, this looks good, at the cost of some accuracy)
+            transform.position = Vector3.Lerp(transform.position, pos, Time.deltaTime * 20);
+            transform.rotation = Quaternion.Lerp(transform.rotation, rot, Time.deltaTime * 20);
+        }
+    }
+}
