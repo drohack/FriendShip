88d24b781defd73f4c3e8258e9fc9372361da818
drohack <drohack@gmail.com>
2016-08-14 19:00:54 -0500

    Toys now using Photon scripts
    Updated the toys to use the Photon Transfrom/Rigidbody View scripts.
    Hopefully they predict better than what I could make.

--- Assets/Photon Lobby/Photon_Transform_Rotation_Script.cs
+++ Assets/Photon Lobby/Photon_Transform_Rotation_Script.cs
@@ -5,11 +5,10 @@ public class Photon_Transform_Rotation_Script : Photon.MonoBehaviour
 {
 
     private bool isRequestingOwnership = false;
-    private Rigidbody rigidBody;
 
     // Use this for initialization
     void Start() {
-        rigidBody = GetComponent<Rigidbody>();
+        
     }
 
     void OnTriggerEnter(Collider other)
@@ -21,41 +20,9 @@ public class Photon_Transform_Rotation_Script : Photon.MonoBehaviour
         }
     }
 
-    void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
-    {
-        if (stream.isWriting)
-        {
-            //We own this player: send the others our data
-            stream.SendNext(transform.position);
-            stream.SendNext(transform.rotation);
-            stream.SendNext(rigidBody.velocity);
-            stream.SendNext(isRequestingOwnership);
-        }
-        else
-        {
-            //Network player, receive data
-            pos = (Vector3)stream.ReceiveNext();
-            rot = (Quaternion)stream.ReceiveNext();
-            vel = (Vector3)stream.ReceiveNext();
-            isRequestingOwnership = (bool)stream.ReceiveNext();
-        }
-    }
-
-    private Vector3 pos = Vector3.zero; //We lerp towards this
-    private Quaternion rot = Quaternion.identity; //We lerp towards this
-    private Vector3 vel = Vector3.zero; //We lerp towards this
-
     // Update is called once per frame
     void LateUpdate()
     {
-        if (!photonView.isMine)
-        {
-            //Update remote player (smooth this, this looks good, at the cost of some accuracy)
-            transform.position = Vector3.Lerp(transform.position, pos, Time.deltaTime * 10);
-            transform.rotation = Quaternion.Lerp(transform.rotation, rot, Time.deltaTime * 10);
-            rigidBody.velocity = vel;
-        }
-
         if(isRequestingOwnership && photonView.isMine)
         {
             isRequestingOwnership = false;
