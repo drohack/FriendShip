23e675508df094631cec7e5cc6c262543a63339f
drohack <drohack@gmail.com>
2016-09-25 23:57:11 -0500

    Removed Mouse interaction from Highlight script
    Removed the highlighting of the mouse which is locked to the center of
    the view in VR.

--- Assets/Scripts/Grabbable/Highlight_Handle_Top_Script.cs
+++ Assets/Scripts/Grabbable/Highlight_Handle_Top_Script.cs
@@ -7,8 +7,7 @@ public class Highlight_Handle_Top_Script : MonoBehaviour {
     private Color startcolor;
     private Color highlightColor = Color.yellow;
     private Renderer topRenderer;
-
-    public bool isMouseOver = false;
+    
     public bool isGrabbing = false;
     public bool isColliding = false;
     int numColliding = 0;
@@ -18,7 +17,6 @@ public class Highlight_Handle_Top_Script : MonoBehaviour {
         Transform topTransform = transform.Find("Top");
         topRenderer = topTransform.GetComponent<Renderer>();
         startcolor = topRenderer.material.color;
-        isMouseOver = false;
         isGrabbing = false;
         isColliding = false;
         numColliding = 0;
@@ -73,36 +71,8 @@ public class Highlight_Handle_Top_Script : MonoBehaviour {
         topRenderer.material.color = startcolor;
     }
 
-    void OnMouseEnter()
-    {
-        isMouseOver = true;
-        if (!Input.GetMouseButton(0))
-        {
-            topRenderer.material.color = highlightColor;
-        }
-    }
-    void OnMouseExit()
-    {
-        isMouseOver = false;
-        if (!isGrabbing)
-            topRenderer.material.color = startcolor;
-    }
-    void OnMouseUp()
-    {
-        isGrabbing = false;
-        if (!isMouseOver)
-            topRenderer.material.color = startcolor;
-    }
-    void OnMouseDown()
-    {
-        isGrabbing = true;
-    }
     void Update()
     {
-        if(isMouseOver && !Input.GetMouseButton(0))
-        {
-            topRenderer.material.color = highlightColor;
-        }
 
         if(isGrabbing && !transform.GetComponent<Grabbable>().isGrabbed)
         {
