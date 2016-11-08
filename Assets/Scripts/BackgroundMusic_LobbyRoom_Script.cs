1adc045e63e96fb23e9a299b74550bc7023f4d71
James-Blond <louis.fairgrieve@gmail.com>
2016-11-07 21:03:09 -0600

    The Music Update
    Added a bunch of free chiptune music from the asset store.  Created an
    audio source object in the game and lobby scenes, which will randomly
    play the music.

---
+++ Assets/Scripts/BackgroundMusic_LobbyRoom_Script.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundMusic_LobbyRoom_Script : MonoBehaviour
+{
+    public AudioClip[] songList;
+    public AudioSource BackGroundMusicSource;
+
+    // Use this for initialization
+    void Start()
+    {
+        songList = new AudioClip[]
+        {
+            (AudioClip)Resources.Load("Music/8Bit Classic Free/Ave Maria"),
+            (AudioClip)Resources.Load("Music/8Bit Classic Free/Courante"),
+            (AudioClip)Resources.Load("Music/8Bit Classic Free/Je te veux"),
+            (AudioClip)Resources.Load("Music/8Bit Classic Free/Pachelbel's Canon"),
+            (AudioClip)Resources.Load("Music/8Bit Classic Free/Pavane for a Dead Princess")
+        };
+
+        BackGroundMusicSource = GetComponent<AudioSource>();
+
+        if (!BackGroundMusicSource.playOnAwake)
+        {
+            BackGroundMusicSource.clip = songList[Random.Range(0, songList.Length)];
+            BackGroundMusicSource.Play();
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!BackGroundMusicSource.isPlaying)
+        {
+            BackGroundMusicSource.clip = songList[Random.Range(0, songList.Length)];
+            BackGroundMusicSource.Play();
+        }
+    }
+}
---
+++ Assets/Scripts/BackgroundMusic_Script.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundMusic_Script : MonoBehaviour {
+    public AudioClip[] songList;
+    public AudioSource BackGroundMusicSource;
+
+    // Use this for initialization
+    void Start ()
+    {
+        songList = new AudioClip[]
+        {
+            (AudioClip)Resources.Load("Music/8-Bit_Action_Free/Aggressive1"),
+            (AudioClip)Resources.Load("Music/8-Bit_Action_Free/Quiet2"),
+            (AudioClip)Resources.Load("Music/8-Bit_Action_Free/Soft1"),
+            (AudioClip)Resources.Load("Music/8-Bit_Action_Free/Stage1"),
+            (AudioClip)Resources.Load("Music/8-Bit_Action_Free/Title")
+        };
+
+        BackGroundMusicSource = GetComponent<AudioSource>();
+
+        if (!BackGroundMusicSource.playOnAwake)
+        {
+            BackGroundMusicSource.clip = songList[Random.Range(0, songList.Length)];
+            BackGroundMusicSource.Play();
+        }
+
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (!BackGroundMusicSource.isPlaying)
+        {
+            BackGroundMusicSource.clip = songList[Random.Range(0, songList.Length)];
+            BackGroundMusicSource.Play();
+        }
+	}
+}
