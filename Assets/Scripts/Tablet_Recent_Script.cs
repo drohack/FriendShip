9f52293c3dae3b47b30eee20214b064ab240551f
drohack <drohack@gmail.com>
2016-11-22 23:06:54 -0600

    Dynamic Tablet_Recenter script
    Depending on the build (Oculus or Vive) the recenter text will change.

---
+++ Assets/Scripts/Tablet_Recent_Script.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tablet_Recent_Script : MonoBehaviour {
+
+    [SerializeField]
+    TextMesh textMesh;
+
+	// Use this for initialization
+	void Start () {
+#if OCULUS
+        textMesh.text = "To Recenter your Camera"
+            + "\nPress in the Thumb Stick";
+#else
+        textMesh.text = "To Recenter your Camera"
+            + "\nPress the top Menu button";
+#endif
+    }
+}
